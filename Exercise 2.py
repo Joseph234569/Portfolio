9a0fa39f540192c1308e9d3ad24e18bca7b692b0
Joseph <josephaustin556@gmail.com>
2023-10-24 14:04:03 -0500

    new code

---
+++ Exercise 2.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you?"))
+
+#simplified comparison - if 16 <= age <= 65:
+if age >= 16 and age <=65:
+    print("Have a good day at work")
+else:
+    print("Enjoy your free time!")
+
+print("_" * 80)
