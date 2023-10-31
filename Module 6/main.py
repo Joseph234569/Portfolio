dd7c180dfe30d0afd438e44551bbb4495d1b0bf6
Joseph <josephaustin556@gmail.com>
2023-10-31 13:57:22 -0500

    new code

---
+++ Module 6/main.py
@@ -0,0 +1,9 @@
+# Import the module
+import my_module
+
+# Use the greet function
+my_module.greet("Alice")
+
+# Create an instance of the Person class
+alice = my_module.Person("Alice")
+alice.say_hello()
---
+++ Module 6/my_module.py
@@ -0,0 +1,11 @@
+# This is a module named my_module
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+class Person:
+    def __init__(self, name):
+     self.name = name
+
+    def say_hello(self):
+        print(f"{self.name} says hello!")
