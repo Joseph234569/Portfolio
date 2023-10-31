dd734c660ca12631e7f8876d479417ea948942ca
Joseph <josephaustin556@gmail.com>
2023-10-31 13:37:40 -0500

    new code

---
+++ Module 6/whiteloop.py
@@ -0,0 +1,13 @@
+# Example 1: Counting from 1 to 5 using a while loop
+
+# Initialize a variable
+
+# Set the condition for the while loop
+while count <=5:
+# Add a condition to skip printing when count is 3
+    count +=1
+# This code will execute as long as the condition (count<=5) is true
+   if count == 3:
+    # this code will execute as long as the condition (count <=5) is true
+    print(count)
+    count +=1
